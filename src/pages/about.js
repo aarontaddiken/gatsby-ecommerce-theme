5b337bbccfd712eaa663ba7f899ead6a0c48c7c4
Aaron <aarontaddiken@gmail.com>
2023-08-10 23:00:27 -0600

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -26,7 +26,7 @@ const AboutPage = (props) => {
         {/* Hero Container */}
         <Hero
           maxWidth={'900px'}
-          image={'/house.png'}
+          image={'/image.png'}
           title={`Rusty \n A metal ass house`}
         />
 
