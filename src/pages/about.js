79a6a90cf1273201648ea2a5606b34b5b600560e
Aaron <aarontaddiken@gmail.com>
2023-08-10 22:44:50 -0600

    Update about.js
    change about image to house image

--- src/pages/about.js
+++ src/pages/about.js
@@ -26,8 +26,8 @@ const AboutPage = (props) => {
         {/* Hero Container */}
         <Hero
           maxWidth={'900px'}
-          image={'/about.png'}
-          title={`Sydney \n A British brand since 1860`}
+          image={'/house.png'}
+          title={`Rusty \n A metal ass house`}
         />
 
         <div className={styles.navContainer}>
